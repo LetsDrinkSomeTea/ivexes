3c9822760011588bef7d4177deaadefc6bd16d20
LetsDrinkSomeTea <github@faigle.dev>
2025-07-15 21:03:14 +0200

    new testcase

---
+++ examples/72_multi_agent_exiftool.py
@@ -0,0 +1,30 @@
+"""Multi-agent example for screen vulnerability analysis."""
+
+import asyncio
+from dotenv import load_dotenv
+
+from ivexes.agents import MultiAgent
+from ivexes.config import PartialSettings, setup_default_logging
+
+load_dotenv(verbose=True, override=True)
+setup_default_logging('WARNING')
+
+settings = PartialSettings(
+    trace_name='Multi-Agent Exiftool',
+    # model='anthropic/claude-sonnet-4-20250514',
+    model='openai/gpt-4.1',
+    reasoning_model='openai/o4-mini',
+    model_temperature=0.1,
+    max_turns=50,
+    embedding_provider='local',
+    embedding_model='intfloat/multilingual-e5-large-instruct',
+    codebase_path='/home/julian/Desktop/Bachelorarbeit/testdata/exiftool/codebase',
+    vulnerable_folder='exiftool-12.23',
+    patched_folder='exiftool-12.24',
+    sandbox_image='vuln-exiftool:latest',
+)
+
+agent = MultiAgent(bin_path='/usr/local/bin/exiftool', settings=settings)
+
+if __name__ == '__main__':
+    asyncio.run(agent.run_interactive())
